3ad997b3f17f139f133797aa871992d8c0b9d241
Leonardo <leonardo.hrjesus@hotmail.com>
2025-07-08 23:55:03 -0300

    INIT PROJECT

---
+++ src/main/kotlin/org/business/lhrjesus/bffappbar/BffAppBarApplication.kt
@@ -0,0 +1,11 @@
+package org.business.lhrjesus.bffappbar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BffappbarApplication
+
+fun main(args: Array<String>) {
+	runApplication<BffappbarApplication>(*args)
+}
---
+++ src/test/kotlin/org/business/lhrjesus/bffappbar/BffappbarApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.business.lhrjesus.bffappbar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BffappbarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
