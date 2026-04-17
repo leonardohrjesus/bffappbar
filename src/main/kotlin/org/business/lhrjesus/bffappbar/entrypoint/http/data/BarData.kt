e83b1c80d270c32b34f309d90895eb58fe6fb518
Leonardo Jesus <leonardo.hrjesus@hotmail.com>
2026-04-17 20:09:44 -0300

    mudança de contrato

--- src/main/kotlin/org/business/lhrjesus/bffappbar/entrypoint/http/data/BarData.kt
+++ src/main/kotlin/org/business/lhrjesus/bffappbar/entrypoint/http/data/BarData.kt
@@ -3,16 +3,16 @@ package org.business.lhrjesus.bffappbar.entrypoint.http.data
 import java.time.LocalDateTime
 
 data class BarData (
-    val name: String,
-    val address: String,
-    val latitude: Double,
-    val date: LocalDateTime,
-    val photoUrl: String,
-    val category: String,
-    val longitude: Double,
-    val descDate: String,
-    val rating : String,
-    val typeEstablishment : String,
-    val videoUrl : String,
-    val linkInstagram : String
+        val name: String,
+        val address: String,
+        val latitude: Double,
+        val date: LocalDateTime,
+        val photoUrl: String,
+        val category: String,
+        val longitude: Double,
+        val descDate: String,
+        val rating : String,
+        val typeEstablishment : String,
+        val videoUrl : String,
+        val linkInstagram : String
 )
